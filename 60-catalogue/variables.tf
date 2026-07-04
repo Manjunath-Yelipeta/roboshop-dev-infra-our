9a528d7e8e8ed435148807b7ac40ebea30885b42
macbookair <macbookair@macbookairs-MacBook-Air.local>
2026-07-04 21:53:03 +0530

    fixing v3

--- 60-catalogue/variables.tf
+++ 60-catalogue/variables.tf
@@ -6,10 +6,14 @@ variable "environment" {
     default = "dev"
 }
 
-/* variable "zone_id" {
+ variable "zone_id" {
     default = "Z07086101C1CVP7AT2UK4"
 }  
 
 variable "domain_name" {
     default = "aiops26.online"
-} */
+} 
+
+variable "app_version" {
+    default = "v3"
+}
